8d9144ec91c693fb3b964d311252d632fd13924b
Maycon Cabral <povei@outlook.com>
2022-12-18 15:01:32 -0300

    doc: CSS [completed]

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useEffect, Suspense, useState } from 'react';
+import React, { useEffect, Suspense } from 'react';
 import { connect } from 'react-redux';
 import { fetchStates } from './redux/actions/state';
 import Loading from './components/Loading';
@@ -10,16 +10,15 @@ const Inputs = React.lazy(() => import('./components/Inputs'));
 
 function App(props) {
   const { dispatch, county, coordinates, zoom, geoJson } = props;  
-
+  
   useEffect(() => {
   dispatch(fetchStates())
-}, [])  
-
+}, [])
 
   return (
     <>
       <main className='home'>
-        <h1>IBGE INFO</h1>
+        <h1 className='title'><span>GEO</span>Município</h1>
         <section className='infoBoard'>
           <div className='infoBoard__inputs'>
             <Suspense fallback={<Loading />}>              
@@ -56,13 +55,15 @@ function App(props) {
               <>
                 <h3>{ county.municipio.nome }</h3>
                 <p><strong>Microregião: </strong>{ county.municipio.microrregiao.nome }</p>
-                <p><strong>Mesorregiao: </strong>{ county.municipio.microrregiao.mesorregiao.nome}</p>                
+                <p><strong>Mesorregião: </strong>{ county.municipio.microrregiao.mesorregiao.nome}</p>
+                <p><strong>UF: </strong>{ county.municipio.microrregiao.mesorregiao.UF.nome}</p>           
               </>
               )
               }
             </div>
           </div>          
-        </section>              
+        </section>
+        <footer><h3><a href='https://www.ibge.gov.br/'>Fonte: IBGE</a></h3></footer>          
       </main>    
     </>
   );
